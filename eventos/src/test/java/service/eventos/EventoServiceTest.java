1b66f25a0292f32b74d8d56a60b4c53f213a23b5
Lucas Pietro Biasi Rayzer <145388833+LucasRayzer@users.noreply.github.com>
2025-10-25 18:39:45 -0300

    add

--- eventos/src/test/java/service/eventos/EventoServiceTest.java
+++ eventos/src/test/java/service/eventos/EventoServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import service.eventos.client.IngressosClient; 
+import service.eventos.client.IngressosClient;
 import service.eventos.client.UserClient;
 import service.eventos.dto.EventoRequisicaoDto;
 import service.eventos.exception.RecursoNaoEncontradoException;
